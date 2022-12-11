34a3a45733b69f369560fe12fe7e89c2c8f620c1
puleugo <puleugo@gmail.com>
2022-12-11 16:56:11 +0900

    docs: 컨트롤러 문서화

--- src/app/post/post.controller.ts
+++ src/app/post/post.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { PostProfileResponse } from '@app/post/dtos/post-profile.response';
 import { PostService } from '@app/post/post.service';
 
 @Controller('posts')
+@ApiTags('게시글')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Get()
+  @ApiOperation({ summary: '현재 인기글 목록을 조회합니다' })
+  @ApiOkResponse({ type: [PostProfileResponse] })
   async getAllPosts(): Promise<[PostProfileResponse]> {
     const posts = await this.postService.getAllPosts();
     // return posts.map((post) => new PostProfileResponse(post));
