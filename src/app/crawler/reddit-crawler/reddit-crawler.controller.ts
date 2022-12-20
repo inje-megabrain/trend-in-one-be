c731dc89f708e3e7adf81a3586174a3393ca042f
puleugo <puleugo@gmail.com>
2022-12-20 19:41:57 +0900

    feat: Reddit 크롤링 기능 구현

--- src/app/crawler/reddit-crawler/reddit-crawler.controller.ts
+++ src/app/crawler/reddit-crawler/reddit-crawler.controller.ts
@@ -6,7 +6,6 @@ import { RedditCrawlerService } from '@app/crawler/reddit-crawler/reddit-crawler
 export class RedditCrawlerController {
   constructor(private readonly redditCrawlerService: RedditCrawlerService) {}
 
-  //TODO: 크롤러에게 직접적인 명령을 내릴 수 있는 API
   @Post()
   async crawlReddit(): Promise<boolean> {
     return this.redditCrawlerService.crawlReddit();
--- src/app/crawler/reddit-crawler/reddit-crawler.module.ts
+++ src/app/crawler/reddit-crawler/reddit-crawler.module.ts
@@ -1,9 +1,27 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { RedditCrawlerController } from './reddit-crawler.controller';
 import { RedditCrawlerService } from './reddit-crawler.service';
 
+import { Community } from '@domain/post/community.entity';
+import { Post } from '@domain/post/post.entity';
+
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Post, Community]),
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      //TODO: baseURL 부분의 서브도메인 변경 코드를 어떻게 작성할지 고민해보기
+      useFactory: (configService: ConfigService) => ({
+        baseURL: '', //https://www.reddit.com
+        validateStatus: () => true,
+      }),
+    }),
+  ],
   controllers: [RedditCrawlerController],
   providers: [RedditCrawlerService],
 })
