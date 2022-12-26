6d05c8e1bfdc70e05f8cef2d9e80bc7181e94758
puleugo <puleugo@gmail.com>
2022-12-26 23:01:20 +0900

    docs: 크롤러 스웨거 문서화

--- src/app/crawler/dc-inside-crawler/dc-inside-crawler.controller.ts
+++ src/app/crawler/dc-inside-crawler/dc-inside-crawler.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { DcInsideCrawlerService } from '@app/crawler/dc-inside-crawler/dc-inside-crawler.service';
 
 @Controller('dc-inside-crawler')
+@ApiTags('[크롤러] DC Inside')
 export class DcInsideCrawlerController {
   constructor(
     private readonly dcInsideCrawlerService: DcInsideCrawlerService,
   ) {}
 
+  @ApiOperation({ summary: 'DC Inside의 현재 인기글 목록 크롤링을 합니다' })
+  @ApiOkResponse({ type: Boolean })
   @Post()
   async crawlDcInside(): Promise<boolean> {
     return this.dcInsideCrawlerService.crawlDcInside();
--- src/infrastructure/swagger/swagger.tags.ts
+++ src/infrastructure/swagger/swagger.tags.ts
@@ -1,5 +1,9 @@
 export const tags: SwaggerTag[] = [
   // 게시글 관련 태그
   { name: '[게시글] 게시글', description: '게시글 관련 기능' },
+
+  // 크롤러 관련 태그
+  { name: '[크롤러] Reddit', description: 'Reddit 크롤러 관련 기능' },
+  { name: '[크롤러] DC Inside', description: 'DC Inside 크롤러 관련 기능' },
 ];
 type SwaggerTag = { name: string; description: string };
