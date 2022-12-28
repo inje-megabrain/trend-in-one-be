66e08d9590a30b4b02be96d44ea8fcb5a2a2242e
puleugo <puleugo@gmail.com>
2022-12-28 14:55:11 +0900

    fix: 커뮤니티 ID 누락 해결

--- src/app/crawler/dc-inside-crawler/dc-inside-crawler.service.ts
+++ src/app/crawler/dc-inside-crawler/dc-inside-crawler.service.ts
@@ -4,6 +4,8 @@ import { Repository } from 'typeorm';
 
 import { PostIngredients } from '@app/crawler/dc-inside-crawler/dc-inside.command';
 import { CrawlerClient } from '@app/crawler/types/crawler.client';
+import { Community } from '@domain/post/community.entity';
+import { CommunityTitle } from '@domain/post/post';
 import { Post } from '@domain/post/post.entity';
 
 @Injectable()
@@ -11,12 +13,24 @@ export class DcInsideCrawlerService {
   constructor(
     @InjectRepository(Post)
     private readonly postRepository: Repository<Post>,
+    @InjectRepository(Community)
+    private readonly communityRepository: Repository<Community>,
     @Inject('DcInsideClient')
     private readonly DcInsideClient: CrawlerClient,
   ) {}
 
   async crawlDcInside(): Promise<boolean> {
     const posts: PostIngredients[] = await this.DcInsideClient.crawl();
+    const communityId = await this.communityRepository.findOne({
+      where: { title: CommunityTitle.DC_INSIDE },
+    });
+    posts.map(async (post) => {
+      const postEntity = this.postRepository.create({
+        ...post,
+        community: communityId,
+      });
+      await this.postRepository.save(postEntity);
+    });
     await this.postRepository.save(posts);
     return true;
   }
