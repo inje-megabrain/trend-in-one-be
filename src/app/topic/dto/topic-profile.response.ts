13f9c395322d92014bb283dd4bce2c1227d37273
puleugo <puleugo@gmail.com>
2022-12-28 13:28:11 +0900

    refactor: 사용하지 않는 프로퍼티 제거

--- src/app/topic/dto/topic-profile.response.ts
+++ src/app/topic/dto/topic-profile.response.ts
@@ -1,21 +1,15 @@
 import { TopicProperties } from '@domain/topic/topic';
 
 export class TopicProfileResponse
-  implements
-    Pick<TopicProperties, 'id' | 'name' | 'query' | 'tweet_volume' | 'url'>
+  implements Pick<TopicProperties, 'name' | 'query' | 'tweet_volume' | 'url'>
 {
-  id: string;
   name: string;
   query: string;
   tweet_volume: number | null;
   url: string;
   constructor(
-    topic: Pick<
-      TopicProperties,
-      'id' | 'name' | 'query' | 'tweet_volume' | 'url'
-    >,
+    topic: Pick<TopicProperties, 'name' | 'query' | 'tweet_volume' | 'url'>,
   ) {
-    this.id = topic.id;
     this.name = topic.name;
     this.query = topic.query;
     this.tweet_volume = topic.tweet_volume;
