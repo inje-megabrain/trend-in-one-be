709bf59593bad3fe84b8fb1b635195990c264fd9
puleugo <puleugo@gmail.com>
2023-01-15 17:09:45 +0900

    refactor: AdminJS 설정 파일 분기

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,12 @@
-import { AdminModule } from '@adminjs/nestjs';
 import * as AdminJSTypeorm from '@adminjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import AdminJS from 'adminjs';
 
 import { ContentsModule } from '@app/contents/contents.module';
+import { ManagementModule } from '@app/management/management.module';
 import { TasksModule } from '@app/tasks/tasks.module';
-import { TasksService } from '@app/tasks/tasks.service';
-import { TopicModule } from '@app/topic/topic.module';
 import { UserModule } from '@app/user/user.module';
-import { VideoModule } from '@app/video/video.module';
-import { Community } from '@domain/post/community.entity';
-import { TaskStatus } from '@domain/task/task';
-import { Task } from '@domain/task/task.entity';
-import { User } from '@domain/user/user.entity';
 
 AdminJS.registerAdapter({
   Resource: AdminJSTypeorm.Resource,
@@ -22,88 +15,11 @@ AdminJS.registerAdapter({
 
 @Module({
   imports: [
-    AdminModule.createAdminAsync({
-      imports: [TasksModule],
-      inject: [TasksService],
-      useFactory: (tasksService: TasksService) => ({
-        adminJsOptions: {
-          rootPath: '/admin',
-          resources: [
-            {
-              resource: User,
-            },
-            {
-              resource: Community,
-            },
-            {
-              resource: Task,
-              options: {
-                actions: {
-                  runTask: {
-                    actionType: 'record',
-                    handler: async (req, res, context) => {
-                      const task = context.record.params;
-                      await tasksService.runTask(
-                        task.id,
-                        task['taskType.title'],
-                        1,
-                      );
-                      task.status = TaskStatus.RUNNING;
-
-                      return {
-                        record: context.record.toJSON(),
-                      };
-                    },
-                    component: false,
-                    icon: 'Play',
-                  },
-
-                  stopTask: {
-                    actionType: 'record',
-                    handler: async (req, res, context) => {
-                      const { record } = context;
-                      const task = record.params;
-                      await tasksService.stopTask(
-                        task.id,
-                        task['taskType.title'],
-                      );
-                      task.status = TaskStatus.STOPPED;
-
-                      return {
-                        record: context.record.toJSON(),
-                        msg: 'Hello world',
-                      };
-                    },
-                    guard: '크롤러를 종료하겠습니까?',
-                    component: false,
-                    icon: 'Stop',
-                  },
-                },
-              },
-            },
-          ],
-          branding: {
-            companyName: 'Trend In One',
-            logo: false,
-          },
-        },
-        auth: {
-          authenticate: async (email, password) =>
-            Promise.resolve({ email: 'puleugo@gmail.com' }),
-          cookieName: 'adminjs',
-          cookiePassword: 'secret',
-        },
-        sessionOptions: {
-          resave: true,
-          saveUninitialized: true,
-          secret: 'secret',
-        },
-      }),
-    }),
     ScheduleModule.forRoot(),
     TasksModule,
     UserModule,
     ContentsModule,
+    ManagementModule,
   ],
 })
 export class AppModule {}
