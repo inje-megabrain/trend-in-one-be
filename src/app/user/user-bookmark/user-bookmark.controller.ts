5f587ec8b02da153e220671d1a2beedf0fe16d55
puleugo <puleugo@gmail.com>
2023-01-27 03:16:49 +0900

    feat: 북마크 기능 엔드포인트 변경

--- src/app/user/user-bookmark/user-bookmark.controller.ts
+++ src/app/user/user-bookmark/user-bookmark.controller.ts
@@ -6,33 +6,34 @@ import {
   ParseUUIDPipe,
   Post,
 } from '@nestjs/common';
-import { ApiOperation, ApiParam } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
 import { UserBookmarkResponse } from '@app/user/user-account/dto/user-bookmark.response';
 import { UserBookmarkService } from '@app/user/user-bookmark/user-bookmark.service';
 
-@Controller('user-account-bookmark')
+@Controller('bookmarks/:userId')
+@ApiTags('[계정] 북마크')
 export class UserBookmarkController {
   constructor(private readonly userBookmarkService: UserBookmarkService) {}
 
-  @Get(':id/bookmarks')
+  @Get(':userId')
   @ApiOperation({ summary: '회원의 북마크 목록을 조회합니다.' })
   @ApiParam({
-    name: 'id',
+    name: 'userId',
     type: String,
     example: 'a6f9cd0d-8cae-414e-bb68-ae9b6d83118d',
   })
   async getUserBookmarks(
-    @Param('id', ParseUUIDPipe) id: string,
+    @Param('userId', ParseUUIDPipe) userId: string,
   ): Promise<UserBookmarkResponse[]> {
-    const bookmarks = await this.userBookmarkService.getUserBookmarks(id);
+    const bookmarks = await this.userBookmarkService.getUserBookmarks(userId);
     return bookmarks.map((bookmark) => new UserBookmarkResponse(bookmark));
   }
 
-  @Post(':id/bookmarks/:postId')
+  @Post(':contentId')
   @ApiOperation({ summary: '회원의 북마크를 추가합니다.' })
   @ApiParam({
-    name: 'id',
+    name: 'userId',
     type: String,
     example: 'a6f9cd0d-8cae-414e-bb68-ae9b6d83118d',
   })
@@ -42,29 +43,32 @@ export class UserBookmarkController {
     example: 'a6f9cd0d-8cae-414e-bb68-ae9b6d83118d',
   })
   async addUserBookmark(
-    @Param('id', ParseUUIDPipe) id: string,
-    @Param('postId', ParseUUIDPipe) postId: string,
+    @Param('id', ParseUUIDPipe) userId: string,
+    @Param('contentId', ParseUUIDPipe) contentId: string,
   ): Promise<UserBookmarkResponse> {
-    const bookmark = await this.userBookmarkService.addUserBookmark(id, postId);
+    const bookmark = await this.userBookmarkService.addUserBookmark(
+      userId,
+      contentId,
+    );
     return new UserBookmarkResponse(bookmark);
   }
 
-  @Delete(':id/bookmarks/:postId')
+  @Delete(':userId/:contentId')
   @ApiOperation({ summary: '회원의 북마크를 삭제합니다.' })
   @ApiParam({
-    name: 'id',
+    name: 'userId',
     type: String,
     example: 'a6f9cd0d-8cae-414e-bb68-ae9b6d83118d',
   })
   @ApiParam({
-    name: 'postId',
+    name: 'contentId',
     type: String,
     example: 'a6f9cd0d-8cae-414e-bb68-ae9b6d83118d',
   })
   async removeUserBookmark(
-    @Param('id', ParseUUIDPipe) id: string,
-    @Param('postId', ParseUUIDPipe) postId: string,
+    @Param('userId', ParseUUIDPipe) userId: string,
+    @Param('contentId', ParseUUIDPipe) contentId: string,
   ): Promise<boolean> {
-    return await this.userBookmarkService.removeUserBookmark(id, postId);
+    return await this.userBookmarkService.removeUserBookmark(userId, contentId);
   }
 }
