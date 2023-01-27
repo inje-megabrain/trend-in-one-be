6f76a0b478a0131426266caa2d36f7a7b0e1e254
puleugo <puleugo@gmail.com>
2023-01-27 14:38:41 +0900

    docs: 스웨거 태그 재명명

--- src/infrastructure/swagger/swagger.tags.ts
+++ src/infrastructure/swagger/swagger.tags.ts
@@ -1,18 +1,18 @@
 export const tags: SwaggerTag[] = [
   // 콘텐츠 관련 태그
   { name: '[콘텐츠] 종합', description: '콘텐츠 관련 기능' },
-  { name: '[콘텐츠] 게시글', description: '게시글 관련 기능' },
-  { name: '[콘텐츠] 영상', description: '영상 관련 기능' },
-  { name: '[콘텐츠] 핫한 주제', description: '트위터 토픽 관련 기능' },
+  { name: '[콘텐츠] 게시글', description: '콘텐츠 관련 기능' },
+  { name: '[콘텐츠] 영상', description: '콘텐츠 관련 기능' },
+  { name: '[콘텐츠] 핫한 주제', description: '콘텐츠 관련 기능' },
 
   // 크롤러 관련 태그
-  { name: '[크롤러] REDDIT', description: 'REDDIT 크롤러 관련 기능' },
-  { name: '[크롤러] DC_INSIDE', description: 'DC_INSIDE 크롤러 관련 기능' },
-  { name: '[크롤러] TWITTER', description: 'TWITTER 크롤러 관련 기능' },
-  { name: '[크롤러] YOUTUBE', description: 'YOUTUBE 크롤러 관련 기능' },
+  { name: '[크롤러] REDDIT', description: '크롤러 관련 기능' },
+  { name: '[크롤러] DC_INSIDE', description: '크롤러 관련 기능' },
+  { name: '[크롤러] TWITTER', description: '크롤러 관련 기능' },
+  { name: '[크롤러] YOUTUBE', description: '크롤러 관련 기능' },
 
-  // 로그인 관련 태그
-  { name: '[인증] 카카오 로그인', description: '카카오 로그인 관련 기능' },
+  // 인증 관련 태그
+  { name: '[인증] 카카오 로그인', description: '인증 관련 기능' },
 
   // 회원 관련 태그
   { name: '[계정] 회원', description: '계정 관련 기능' },
